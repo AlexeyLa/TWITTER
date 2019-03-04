7b984d5a27a3488cab3e0bb0447ba4119ee435d7
AlexeyLa <alexeyla@mail.ru>
2019-03-04 21:53:34 +0300

    Add files via upload

--- text_analysis.py
+++ text_analysis.py
@@ -19,44 +19,59 @@ BAD_SYMBOLS_RE = re.compile('[^0-9a-z #+_]')
 
 file_object = open('t_don.txt', 'r') 
 counter = 0
-for km in range(1,5):
-    new_file = open(str(km) + '.txt', 'w')
-    for i in file_object:
-        if (counter < 1500) and (i != '\n'):
-            new_text = re.sub(REPLACE_BY_SPACE_RE, ' ', i)
-            counter+=1;
-            print(km, ": ", counter)
-            new_file.write(new_text)
-    new_file.close()
-        
+num_lines = 1000
+km = 0
 
+# split txt file
 
-file_object  = open('t_don.txt', 'r') 
-part = []
-counter = 0
-words_type = []
-result_dict = {}
+file_part = open('0.txt', 'w')
 for i in file_object:
-    if (counter < 500) and (i != '\n'):
+    if (i != '\n'):
         new_text = re.sub(REPLACE_BY_SPACE_RE, ' ', i)
         counter+=1;
-        print(counter)
-        for k in new_text:
-            p2 = morph.parse(k)[0].tag.POS
-            if (p2 != None):
-                if (p2 in result_dict.keys()):
-                    result_dict[p2]+=1
-                else:
-                    result_dict[p2] = 1
+        print(km, ": ", counter)
+        file_part.write(new_text)
+        if (counter > num_lines):
+            file_part.close()
+            km+=1
+            print(km)
+            file_part = open(str(km) + '.txt', 'w')
+            counter = 0       
+file_object.close()
+    
+dict_full_list = []
+for ft in range(0,20):
+    file_object  = open(str(ft) + '.txt', 'r') 
+    part = []
+    counter = 0
+    words_type = []
+    result_dict = {}
+    for i in file_object:
+        if (counter < num_lines) and (i != '\n'):
+            new_text = re.sub(REPLACE_BY_SPACE_RE, ' ', i)
+            counter+=1;
+            print(counter)
+            for k in new_text:
+                p2 = morph.parse(k)[0].tag.POS
+                if (p2 != None):
+                    if (p2 in result_dict.keys()):
+                        result_dict[p2]+=1
+                    else:
+                        result_dict[p2] = 1
+    dict_full_list.append(result_dict)
+    file_object.close()
 
-markers = ['* ЧАСТЬ ПЕРВАЯ *', '* ЧАСТЬ ВТОРАЯ *', '* ЧАСТЬ ТРЕТЬЯ *']
+
 #result_dict = {i:words_type.count(i) for i in words_type}
-
-pos_list = []
-freq_pos = []
-for key, value in result_dict.items():
-    pos_list.append(key)
-    freq_pos.append(value)
+freq_list = []
+for k in range(0,20):
+    result_dict = dict_full_list[k]
+    pos_list = []
+    freq_pos = []
+    for key, value in result_dict.items():
+        pos_list.append(key)
+        freq_pos.append(value)
+    freq_list.append(freq_pos)
 
 # DISPLAY RESULTS
 
